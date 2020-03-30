f4a83f6355fec19dbfde9f74692b2f9f1461f3b0
jumi sanchez <jumary.sanchez@codeimmersives.com>
2020-03-29 20:29:45 -0400

    todata

--- main.js
+++ main.js
@@ -1,22 +1,56 @@
 /********************
  * HELPER FUNCTIONS *
  ********************/
-const getTodoName = function() {}
-const getCompleteness = function() {}
-const getPriority = function() {}
-const isComplete = function() {}
-const isHighPriority = function() {}
+const getTodoName = function(names) {
+  return names.text
+}
+const getCompleteness = function(completeness) {
+  if(completeness.complete){
+    return true
+  }else{
+    return false
+  }
+}
+const getPriority = function(complete) {
+  return complete.priority
+}
+const isComplete = function(completeness) {
+  if(completeness.complete){
+    return true
+  }else{
+    return false
+  }
+}
+const isNotComplete = function(not){
+return !not.complete
+}
+const isHighPriority = function(standards) {
+    return standards.priority > 1 }
+    
+
 
 /***********************
  * ITERATION FUNCTIONS *
  ***********************/
-const names = function() {}
-const priorities = function() {}
-const namesAndPriorities = function() {}
-const justNotComplete = function() {}
-const justComplete = function() {}
+const names = function(name) {
+  return name.map(getTodoName)
+}
+const priorities = function(priority) {
+  return priority.map(getPriority)
+}
+const namesAndPriorities = function(both) {
+  return both.map()
+}
+const justNotComplete = function(array) {
+  return array.filter(isNotComplete)
+
+}
+const justComplete = function(array) {
+  return array.filter(isComplete)
+}
 const priority2Only = function() {}
 const priority1Only = function() {}
+
 
 
 module.exports = {
